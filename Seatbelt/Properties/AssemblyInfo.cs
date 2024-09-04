7a4221d075d73868cef815ca87e88148d1dfa3b7
Alex Rodchenko <gam4er@gmail.com>
2024-09-04 11:23:13 +0300

    Upd
    -Assembly info
    +up langver

--- Seatbelt/Properties/AssemblyInfo.cs
+++ Seatbelt/Properties/AssemblyInfo.cs
@@ -1,15 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Seatbelt")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Seatbelt")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("aec32155-d589-4150-8fe7-2900df4554c8")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
